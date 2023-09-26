a9d44a32e076a615ce5442f40cfe8e6eed749d70
JoePereira <joedersonpereeira@gmail.com>
2023-09-26 11:52:53 -0300

    imprementando o metodo transferir e utilizando a mesma

--- src/main.kt
+++ src/main.kt
@@ -6,7 +6,7 @@ fun main() {
     contaJoe.saldo = 200.0
 
     val contaJuli = Conta()
-    contaJuli.titular = "Jujuba"
+    contaJuli.titular = "Juli"
     contaJuli.numero = 1001
     contaJuli.saldo = 300.0
 
@@ -19,7 +19,14 @@ fun main() {
     println(contaJoe.numero)
     println(contaJoe.saldo)
     contaJoe.deposita(70.0)
-    contaJoe.saca(50.0)
+    contaJoe.saca(270.0)
+
+    if(contaJuli.transfere(100.0, contaJoe)){
+        println("Trnasferência concluída com sucesso")
+    }else {
+        println("Falha na trnasferência")
+    }
+
 }
 
 class Conta {
@@ -28,19 +35,31 @@ class Conta {
     var saldo = 0.0
 
     fun deposita(valor: Double) {
-        println("Depositando na conta do ${titular}")
+        println("Depositando na conta de ${titular}")
         this.saldo += valor
         println(saldo)
     }
 
     fun saca(valor: Double) {
         if(saldo >= valor){
-            println("Sacando $valor da conta do ${titular}")
+            println("Sacando $valor da conta de ${titular}")
             this.saldo -= valor
             println(saldo)
         } else{
-            println("Saldo insuficiente")
+            println("Não foi possível sacar $valor. Saldo insuficiente")
         }
+    }
+
+    fun transfere(valor: Double, destino: Conta): Boolean{
+        if(saldo >= valor) {
+            println("Transferência da conta de $titular para ${destino.titular}")
+            saldo -= valor
+            destino.saldo += valor
+            println("Saldo conta $titular: $saldo")
+            println("Saldo conta ${destino.titular}: ${destino.saldo}")
+            return true
+        }
+        return false
     }
 
 
