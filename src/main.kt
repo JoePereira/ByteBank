c9040b68933b2fc682ff9a92ab097f11cf5d1d6c
JoePereira <joedersonpereeira@gmail.com>
2023-09-26 11:23:32 -0300

    implementando o metodo depositar e utilizando a mesma

--- src/main.kt
+++ src/main.kt
@@ -13,24 +13,24 @@ fun main() {
     println(contaJuli.titular)
     println(contaJuli.numero)
     println(contaJuli.saldo)
-    deposita(contaJuli, 50.0)
+    contaJuli.deposita(contaJuli, 50.0)
 
     println(contaJoe.titular)
     println(contaJoe.numero)
     println(contaJoe.saldo)
-    deposita(contaJoe, 70.0)
-}
-
-fun deposita(conta: Conta, valor: Double) {
-    println("Depositando na conta do ${conta.titular}")
-    conta.saldo += valor
-    println(conta.saldo)
+    contaJoe.deposita(contaJoe, 70.0)
 }
 
 class Conta {
     var titular = ""
     var numero = 0
     var saldo = 0.0
+
+    fun deposita(conta: Conta, valor: Double) {
+        println("Depositando na conta do ${conta.titular}")
+        conta.saldo += valor
+        println(conta.saldo)
+    }
 
 }
 
