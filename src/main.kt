71acad47fd8d776938bda6a8f6ccb786dfc9571b
JoePereira <joedersonpereeira@gmail.com>
2023-09-26 09:55:57 -0300

    criando o objeto de Conta e populando com algumas informações

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,22 @@
 fun main() {
     println("Bem vindo ao ByteBank")
+    val contaJoe = Conta()
+    contaJoe.titular = "Joe"
+    contaJoe.numero = 1000
+    contaJoe.saldo = 200.0
 
+    val contaJuli = Conta()
+    contaJuli.titular = "Jujuba"
+    contaJuli.numero = 1001
+    contaJuli.saldo = 300.0
+
+    println(contaJuli.titular)
+    println(contaJuli.numero)
+    println(contaJuli.saldo)
+
+    println(contaJoe.titular)
+    println(contaJoe.numero)
+    println(contaJoe.saldo)
 }
 
 class Conta {
