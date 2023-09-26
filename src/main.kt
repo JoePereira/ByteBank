33456f4122ef14fc256b04ae93dafac189da8133
JoePereira <joedersonpereeira@gmail.com>
2023-09-26 12:07:49 -0300

    implementando getters e setters na classe Conta

--- src/main.kt
+++ src/main.kt
@@ -3,36 +3,36 @@ fun main() {
     val contaJoe = Conta()
     contaJoe.titular = "Joe"
     contaJoe.numero = 1000
-    contaJoe.saldo = 200.0
+    contaJoe.setSaldo(-200.0)
 
     val contaJuli = Conta()
     contaJuli.titular = "Juli"
     contaJuli.numero = 1001
-    contaJuli.saldo = 300.0
+    contaJuli.setSaldo(300.0)
 
     println(contaJuli.titular)
     println(contaJuli.numero)
-    println(contaJuli.saldo)
-    contaJuli.deposita(50.0)
+    println(contaJuli.getSaldo())
+//    contaJuli.deposita(50.0)
 
     println(contaJoe.titular)
     println(contaJoe.numero)
-    println(contaJoe.saldo)
-    contaJoe.deposita(70.0)
-    contaJoe.saca(270.0)
-
-    if(contaJuli.transfere(100.0, contaJoe)){
-        println("Trnasferência concluída com sucesso")
-    }else {
-        println("Falha na trnasferência")
-    }
+    println(contaJoe.getSaldo())
+//    contaJoe.deposita(70.0)
+//    contaJoe.saca(270.0)
+//
+//    if(contaJuli.transfere(100.0, contaJoe)){
+//        println("Trnasferência concluída com sucesso")
+//    }else {
+//        println("Falha na trnasferência")
+//    }
 
 }
 
 class Conta {
     var titular = ""
     var numero = 0
-    var saldo = 0.0
+    private var saldo = 0.0
 
     fun deposita(valor: Double) {
         println("Depositando na conta de ${titular}")
@@ -54,12 +54,23 @@ class Conta {
         if(saldo >= valor) {
             println("Transferência da conta de $titular para ${destino.titular}")
             saldo -= valor
-            destino.saldo += valor
+            destino.deposita(valor)
             println("Saldo conta $titular: $saldo")
             println("Saldo conta ${destino.titular}: ${destino.saldo}")
             return true
         }
         return false
+    }
+
+    fun getSaldo(): Double {
+        return saldo
+    }
+
+    fun setSaldo(valor: Double) {
+        if(valor > 0){
+            saldo = valor
+        }
+
     }
 
 
